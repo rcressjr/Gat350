a70447e47d63003c23ef21ba4314723494732b3a
Richard Cress <rcressjr@student.neumont.edu>
2021-10-06 10:05:14 -0600

    Add project files.

---
+++ Source/Main.cpp
@@ -0,0 +1,42 @@
+#include "Renderer.h"
+
+#include <iostream>
+#include <SDL.h>
+
+int main(int, char**) {
+	const int WIDTH = 800;
+	const int HEIGHT = 600;
+
+	std::unique_ptr<Renderer> renderer = std::make_unique<Renderer>();
+	renderer->Initialize(WIDTH, HEIGHT);
+	
+	std::unique_ptr<Framebuffer> framebuffer = std::make_unique<Framebuffer>(renderer.get(), renderer->width, renderer->height);
+
+	bool quit = false;
+	SDL_Event event;
+	while (!quit) {
+
+		SDL_PollEvent(&event);
+		switch (event.type) {
+		case SDL_QUIT:
+			quit = true;
+			break;
+		}
+
+		framebuffer->Clear(color_t{ 0, 0, 0, 0 });
+
+		for (int i = 0; i < 100; i++) {
+			framebuffer->DrawPoint(rand() % framebuffer->width, rand() % framebuffer->height, { 255, 255, 255, 255 });
+		}
+
+		framebuffer->Update();
+
+		renderer->CopyBuffer(framebuffer.get());
+
+		renderer->Present();
+	}
+
+	SDL_Quit();
+
+	return 0;
+}
